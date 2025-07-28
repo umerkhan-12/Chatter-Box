55429f0c583930f948ef6f94e52d69e1602454a5
Umer khan <khaankhattack321@gmail.com>
2025-07-28 16:51:50 +0500

    Add speed factor

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import Navbar from "@/components/Navbar";
 import {
   ClerkProvider,
@@ -35,6 +36,7 @@ export default function RootLayout({ children }) {
       
         <Navbar/>
         {children}
+        <SpeedInsights />
         <Analytics />
       </body>
      </html>
