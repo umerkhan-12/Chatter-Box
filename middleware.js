7d1f8e5920466c4f360a6ad0f1c26c21bcd7edfd
Umer khan <khaankhattack321@gmail.com>
2025-08-04 16:12:45 +0500

    Upadte middle ware

--- middleware.js
+++ middleware.js
@@ -1,41 +1,21 @@
-// import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
-
-// const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)','/api/create'])
-
-// export default clerkMiddleware(async (auth, req) => {
-//   if (!isPublicRoute(req)) await auth.protect()
-// })
-
-
-
-// export const config = {
-//   matcher: [
-//     // Skip Next.js internals and all static files, unless found in search params
-//     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-//     // Always run for API routes
-//     '/(api|trpc)(.*)',
-//   ],
-// };
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher([
   '/',
   '/sign-in(.*)',
   '/sign-up(.*)',
-  '/api/create'
-])
+  '/api/create',
+]);
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) return auth().protect()
-})
+  if (!isPublicRoute(req)) {
+    return auth().protect();
+  }
+});
 
 export const config = {
   matcher: [
-    /*
-     * Match all routes except:
-     * - _next (Next.js internals)
-     * - static files
-     */
     '/((?!_next|.*\\..*).*)',
+    '/(api|trpc)(.*)',
   ],
-}
+};
